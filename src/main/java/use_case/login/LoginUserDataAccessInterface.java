7ca24d45bb0b10564bd90429aed80ad49684148f
sHin7823 <migz.galang@mail.utoronto.ca>
2024-10-17 15:09:03 -0400

    Task 2.2 done

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the user with the given name.
+     * @param name username.
+     */
+    void setCurrentUser(String name);
 }
